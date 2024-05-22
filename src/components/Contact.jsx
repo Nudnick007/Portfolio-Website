ece05741e2ce7c79be67b69670d8deb662a41220
Nudnick007 <97278688+Nudnick007@users.noreply.github.com>
2024-05-22 20:19:22 +0530

    contact buttons fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//template_fpvyzjl
+// service_o4zn68o
+//erSOI82WWW1updQZf
+
 const Contact = () => {
   const formRef = useRef();
   const [form,setForm] = useState({
@@ -17,8 +21,43 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
   
-  const handleChange = (e) =>{}
-  const handleSubmit = (e) => {}
+  const handleChange = (e) =>{
+    const {name,value} = e.target;
+    setForm({...form,[name]:value})
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send('service_o4zn68o','template_fpvyzjl',
+      {
+        from_name: form.name,
+        to_name: 'Nikhil',
+        from_email: form.email,
+        to_email: 'nikhiljoshi71120@gmail.com',
+        message: form.message,
+      },
+      'erSOI82WWW1updQZf'
+    )
+    .then(
+      () => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
+
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      },
+      (error) => {
+        setLoading(false);
+        console.log(error);
+
+        alert("Ahh, something went wrong. Please try again.");
+      }
+    );
+  }
 
   return (
     <div
